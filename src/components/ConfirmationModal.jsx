f7498cc65aac013b0e7d6796f8967d1e613564c2
TechStackInb <coding@inboundsys.com>
2024-09-26 16:48:16 +0530

    Updating css

--- src/components/ConfirmationModal.jsx
+++ src/components/ConfirmationModal.jsx
@@ -6,9 +6,9 @@ const ConfirmationModal = ({ isOpen, onClose, onConfirm }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white rounded-lg p-8 shadow-lg w-1/3">
-        <h2 className="text-lg font-bold mb-4">Confirm Deletion</h2>
-        <p className="mb-6">Are you sure you want to delete this policy?</p>
+      <div className="bg-gray-800 rounded-lg p-8 shadow-lg w-1/3">
+        <h2 className="text-lg font-bold mb-4 text-white">Confirm Deletion</h2>
+        <p className="mb-6 text-white">Are you sure you want to delete this policy?</p>
         <div className="flex justify-end">
           <button
             className="bg-gray-300 text-black px-4 py-2 rounded mr-2"
