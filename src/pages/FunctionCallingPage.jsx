a5a6c5acfaf05c4c95eb6de1cf661ec73f20a7c7
TechStackInb <coding@inboundsys.com>
2024-11-08 17:34:47 +0530

    Changesfunction calling

--- src/pages/FunctionCallingPage.jsx
+++ src/pages/FunctionCallingPage.jsx
@@ -3822,38 +3822,7 @@ const FunctionCalling = () => {
                                                 </span>
                                               </div>
                                             </div>
-                                            <div className="border-t border-gray-600"></div>
-                                            <div className="flex ">
-                                              <div
-                                                className="flex items-center justify-center text-[black] bg-gray-700 rounded-full"
-                                                style={{
-                                                  width: '47px',
-                                                  height: '47px',
-                                                  background: '#FFFFFF',
-                                                  opacity: 1,
-                                                }}
-                                              >
-                                                <img
-                                                  src={userIcon}
-                                                  alt="icons"
-                                                  style={{
-                                                    width: '47px',
-                                                    height: '47px',
-                                                  }}
-                                                />
-                                              </div>
-                                              <div className="flex flex-col ml-3">
-                                                <span className="text-white block text-base font-poppins font-semibold">
-                                                  Bob Rogers
-                                                </span>
-                                                <span className="text-gray-400 text-sm font-poppins font-normal">
-                                                  Member{' '}
-                                                  <FontAwesomeIcon
-                                                    icon={faAngleDown}
-                                                  />
-                                                </span>
-                                              </div>
-                                            </div>
+                                        
                                             <div className="border-t border-gray-600"></div>
                                             <div className="flex ">
                                               <div
@@ -4499,38 +4468,7 @@ const FunctionCalling = () => {
                                                   </span>
                                                 </div>
                                               </div>
-                                              <div className="border-t border-gray-600"></div>
-                                              <div className="flex ">
-                                                <div
-                                                  className="flex items-center justify-center text-[black] bg-gray-700 rounded-full"
-                                                  style={{
-                                                    width: '47px',
-                                                    height: '47px',
-                                                    background: '#FFFFFF',
-                                                    opacity: 1,
-                                                  }}
-                                                >
-                                                  <img
-                                                    src={userIcon}
-                                                    alt="icons"
-                                                    style={{
-                                                      width: '47px',
-                                                      height: '47px',
-                                                    }}
-                                                  />
-                                                </div>
-                                                <div className="flex flex-col ml-3">
-                                                  <span className="text-white block text-base font-poppins font-semibold">
-                                                    Bob Rogers
-                                                  </span>
-                                                  <span className="text-gray-400 text-sm font-poppins font-normal">
-                                                    Member{' '}
-                                                    <FontAwesomeIcon
-                                                      icon={faAngleDown}
-                                                    />
-                                                  </span>
-                                                </div>
-                                              </div>
+                                        
 
                                               <div className="border-t border-gray-600"></div>
                                               <div className="flex ">
