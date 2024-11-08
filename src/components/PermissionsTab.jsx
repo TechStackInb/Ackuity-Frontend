00609914507c45102a02f72304e18db9e7dc0679
TechStackInb <coding@inboundsys.com>
2024-11-08 17:38:23 +0530

    Changesfunction permission

--- src/components/PermissionsTab.jsx
+++ src/components/PermissionsTab.jsx
@@ -829,36 +829,6 @@ function PermissionsTab() {
                                         </span>
                                       </div>
                                     </div>
-                                    <div className="border-t border-gray-600"></div>
-                                    <div className="flex ">
-                                      <div
-                                        className="flex items-center justify-center text-[black] bg-gray-700 rounded-full"
-                                        style={{
-                                          width: '47px',
-                                          height: '47px',
-                                          background: '#FFFFFF',
-                                          opacity: 1,
-                                        }}
-                                      >
-                                        <img
-                                          src={userIcon}
-                                          alt="icons"
-                                          style={{
-                                            width: '47px',
-                                            height: '47px',
-                                          }}
-                                        />
-                                      </div>
-                                      <div className="flex flex-col ml-3">
-                                        <span className="text-white block text-base font-poppins font-semibold">
-                                          Bob Rogers
-                                        </span>
-                                        <span className="text-gray-400 text-sm font-poppins font-normal">
-                                          Member{' '}
-                                          <FontAwesomeIcon icon={faAngleDown} />
-                                        </span>
-                                      </div>
-                                    </div>
 
                                     <div className="border-t border-gray-600"></div>
                                     <div className="flex ">
